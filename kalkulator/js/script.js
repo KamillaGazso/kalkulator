176ec7ceb257e50958c491c03ad7c8f0f016f172
Kamillagazso <kamillagazso04@gmail.com>
2022-01-11 13:55:57 +0100

    ize

--- kalkulator/js/script.js
+++ kalkulator/js/script.js
@@ -1,11 +1,28 @@
 function szamol() {
     let szam1Szoveg=document.getElementById("szam1").value
-
-    console.log( `Első szám: ${szam1Szoveg}`)
-
     let szam2Szoveg=document.getElementById("szam2").value
+    let muv=document.getElementById("muvelet").value
 
-    console.log(`második szám: ${szam2Szoveg}`)
+
+
+    let x=parseInt(szam1Szoveg)
+    let y=parseInt(szam2Szoveg)
+    let eredmeny=0
+    
+    if (muv === "osszead"){
+        eredmeny= x + y
+
+    } else if (muv === "kivon"){
+        eredmeny= x - y
+
+    } else if (muv === "oszt"){
+        eredmeny=x/y
+
+    } else if (muv === "szoroz"){
+        eredmeny=x*y
+
+    }
+    console.log(eredmeny)
 }
 
 document.getElementById("szam1").focus()
